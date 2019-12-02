afb0dbbb297f1af49de5d100c8df977153a1dd35
jsjrjz08 <805054qaz>
2019-12-02 23:51:59 +0800

    6种常见排序算法

--- 00-Basement/src/Main.java
+++ 00-Basement/src/Main.java
@@ -1,26 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Tools.intToString(38294));
-        System.out.println(Tools.intToString(-38294));
+//        System.out.println(Tools.intToString(38294));
+//        System.out.println(Tools.intToString(-38294));
+//
+//        System.out.println(Tools.intToString(0));
+//        System.out.println(Tools.intToString(-0));
+//
+//        System.out.println(Tools.intToString(638294));
+//        System.out.println(Tools.intToString(-638294));
+//
+//        System.out.println(Tools.isPrime(-3));
+//        System.out.println(Tools.isPrime(0));
+//        System.out.println(Tools.isPrime(2));
+//        System.out.println(Tools.isPrime(3));
+//        System.out.println("9:"+Tools.isPrime(9));
+//        System.out.println(Tools.isPrime(13));
+//        System.out.println(Tools.isPrime(14));
+//
+//        System.out.println(Tools.pow(2,3));
+//        System.out.println(Tools.pow(2,-3));
+//
+//        System.out.println(Tools.binarySearch01(GenerateArrayUtil.generateOrderedArray(30),4));
+//        System.out.println(Tools.binarySearch02(GenerateArrayUtil.generateOrderedArray(30),4));
 
-        System.out.println(Tools.intToString(0));
-        System.out.println(Tools.intToString(-0));
-
-        System.out.println(Tools.intToString(638294));
-        System.out.println(Tools.intToString(-638294));
-
-        System.out.println(Tools.isPrime(-3));
-        System.out.println(Tools.isPrime(0));
-        System.out.println(Tools.isPrime(2));
-        System.out.println(Tools.isPrime(3));
-        System.out.println("9:"+Tools.isPrime(9));
-        System.out.println(Tools.isPrime(13));
-        System.out.println(Tools.isPrime(14));
-
-        System.out.println(Tools.pow(2,3));
-        System.out.println(Tools.pow(2,-3));
-
-        System.out.println(Tools.binarySearch01(GenerateArrayUtil.generateOrderedArray(30),4));
-        System.out.println(Tools.binarySearch02(GenerateArrayUtil.generateOrderedArray(30),4));
+//        int[] arr = {6,9,7,8,6,10,3};
+        System.out.print("冒泡排序：");
+        Tools.bubbleSort(new int[]{6,9,7,8,6,10,3});
+        System.out.print("选择排序：");
+        Tools.selectionSort(new int[]{6,9,7,8,6,10,3});
+        System.out.print("插入排序：");
+        Tools.insertionSort(new int[]{6,9,7,8,6,10,3});
+        System.out.print("快速排序：");
+        Tools.quickSort(new int[]{6,9,7,8,6,10,3});
+        System.out.print("归并排序：");
+        Tools.mergeSort(new int[]{6,9,7,8,6,10,3});
+        System.out.print("堆排序：");
+        Tools.heapSort(new int[]{6,9,7,8,6,10,3});
     }
+
 }
--- 00-Basement/src/Tools.java
+++ 00-Basement/src/Tools.java
@@ -57,30 +57,182 @@ class Tools {
         return -1;
     }
 
-    //选择排序
-
-    //归并排序
-
-    //快速排序 -- 升序
-    public static void quickSort(int[] arr) {
-        int len = arr.length;
-        if(len == 0 || len == 1) {
+    //冒泡排序
+//    1. 从0位置开始，比较相邻两个数的大小，如果后面的数小于前面，则交换位置。
+//    2. 遍历一遍下来，最后一个数为整个数组中的最大值。
+//    3. 把最后一个数排除，继续比较剩下的数组。
+//    4. 总共比较次数为N*N，时间复杂度为O（n²）。
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
             return;
         }
-
-        int left = 0;//数组下标左边界
-        int right = len - 1;//数组下标右边界
-        //标的点，初始化为最左侧元素
-        int pivot = arr[left];
-        //(left,k]区间为小于等于pivot的元素下标。初始化为left，此时，(left,k]区间没有任何元素
-        int k = left;
-
-        for(int i=left+1;i<=right;i++) {
-            if(arr[i] <= pivot) {
-                k++;
-                swap(arr,k,i);
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
             }
-            //(k,i)之间是大于pivot的元素
+        }
+        printArray(arr);
+    }
+
+    //选择排序
+//    1.遍历一遍，找到整个数组中最小的数，与位置0的数交换位置。
+//    2.从1位置开始，继续遍历，找到最小的数，与1位置交换。以此类推。
+//    3.同冒泡排序，复杂度为O（n²）。
+    public static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr, i, minIndex);
+        }
+        printArray(arr);
+    }
+
+    //插入排序
+//    1.从1位置开始，比较与前面数的大小，如果小于前面的数，则交换位置，直到不再小于停止。
+//    2.接着从2位置开始，重复这个过程。直到最后位置为止。
+//    3.时间复杂度取决于数组的排序情况，当数组基本有序时候，复杂度很低，接近O（n）。当数组完全无序时，每个数都要经过多次移动，复杂度趋近于O（n²）。
+    public static void insertionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j + 1);
+            }
+        }
+        printArray(arr);
+    }
+
+    //希尔排序
+    //1. 过程类似于插入排序，算是插入排序的一种优化。
+    //2. 首先，需要确定一个步长k，根据步长，把数组分为N/k部分，每一部分单独排序。
+    //3. 把步长缩短，继续排序，直到步长为1。
+    //4. 通过步长，减少了数组需要移动的次数，从而降低了复杂度。
+    //5. 所以复杂度的高低完全取决于步长的好坏，是一种特别不稳定的算法，也是一种实现简单分析困难的算法。
+
+    //快速排序 -- 升序
+//    快速排序如果每次都选到最大值，或者最小值，就会产生最坏的情况，使复杂度达到O（n²）级别。但是可以通过随机选择partition值，从数学期望上避免这种情况的发生。所以可以默认其复杂度为O（N * lg N）。
+//    1.随机选出一个partition值，把大于partition值的放在它右边，小于它的放在它左边。
+//    2.从partition值的左右两边分割，调用自己，开始递归。
+//    3.这里有一点优化，因为partition值在数组中可能不止一个，因此返回一个长度为2的数组，代表partition的左右边界，从边界两端进行递归，更加快速。
+    public static void quickSort(int[] arr) {
+        if ( arr == null || arr.length < 2) {
+            return;
+        }
+        quickSort(arr, 0, arr.length-1);
+        printArray(arr);
+    }
+
+    public static void quickSort(int[] arr, int left, int right) {
+        if ( left >= right ) {
+            return;
+        }
+        //随机产生partition值，防止出现最坏情况
+        swap(arr, right, (int) (Math.random() * ( right - left + 1)  )+ left );
+        //返回的数组p为partition的左右边界
+        int[] p = partition(arr, left, right);
+        quickSort(arr, left, p[0]-1);
+        quickSort(arr, p[1]+1, right);
+    }
+
+    public static int[] partition(int[] arr, int left, int right) {
+        int less = left - 1, more = right;
+        while ( left < more ) {
+            if ( arr[left] < arr[right] ) {
+                swap(arr, ++less, left++);
+            } else if ( arr[left] > arr[right] ) {
+                swap(arr, --more, left);
+            } else {
+                left++;
+            }
+        }
+        swap(arr, more, right);
+        return new int[] {less+1,more};
+    }
+
+    //归并排序
+//    1.把数组分成两部分，分别比较大小，最后合并。
+//    2.递归调用自己
+    public static void mergeSort(int[] arr) {
+        if ( arr == null || arr.length < 2 ) {
+            return;
+        }
+        mergeSort(arr, 0, arr.length - 1 ) ;
+        printArray(arr);
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m + 1;
+        while (p1 <= m && p2 <= r) {
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+    }
+
+    //堆排序
+//    1.首先用数组构造一个大根堆。
+//    2.把大根堆队顶和最后位置的元素交换位置，最后一个元素脱离大根堆，即数组长度减一。
+//    3.队顶下沉，再次构造大根堆，重复这个过程，直至完全排序。
+    public static void heapSort(int[] arr) {
+        if ( arr == null || arr.length < 2 ) {
+            return;
+        }
+        for ( int i = 1; i < arr.length; i++ ) {
+            insertHeap(arr, i);
+        }
+        int r = arr.length - 1;
+        while(r > 0 ) {
+            swap(arr, 0, r);
+            heapify(arr, 0, r--);
+        }
+        printArray(arr);
+    }
+
+    public static void insertHeap(int[] arr, int i) {
+        while (arr[i] > arr[(i - 1) / 2]) {
+            swap(arr, i, (i - 1) / 2);
+            i = (i - 1) / 2;
+        }
+    }
+
+    public static void heapify(int[] arr, int index, int size) {
+        int left = index * 2 + 1;
+        while ( left < size ) {
+            int largest = left+1 < size && arr[left] < arr[left + 1] ? left+1 : left;
+            if ( arr[index] > arr[largest] ) {
+                return;
+            }
+            swap(arr, index, largest);
+            index = largest;
+            left = index * 2 + 1;
         }
     }
 
@@ -157,5 +309,18 @@ class Tools {
             result += s.charAt(k);
         }
         return num < 0 ? "-"+result : result;
+    }
+
+    public static void printArray(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for(int i=0;i<arr.length;i++) {
+            sb.append(arr[i]);
+            if(i != arr.length-1) {
+                sb.append(",");
+            }
+        }
+        sb.append("]");
+        System.out.println(sb.toString());
     }
 }
